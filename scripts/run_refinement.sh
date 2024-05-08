6756ed2278ac209ab4365910b57ca1d4e1d57ab2
KeremZaman <kerem_zaman@hotmail.com>
2024-05-08 13:20:30 -0400

    Add scripts to run with lesson and refinement methods

---
+++ scripts/run_refinement.sh
@@ -0,0 +1,23 @@
+CATEGORY=$1
+news_articles=("business" "entertainment" "oddities" "politics" "science" "sports" "us-news" "world-news")
+academic_papers=("cs" "econ" "eess" "math" "physics" "q-bio" "q-fin" "stat")
+
+if [ "$CATEGORY" == "news_articles" ]; then
+    for SUBCAT in "${news_articles[@]}"
+    do
+        #echo -e "$CATEGORY/$SUBCAT"
+        python 5-Dynamic/dynamic.py --context $CATEGORY --num-turns 5 --refine-questions --context-folder data/a_files/$CATEGORY/$SUBCAT --root-folder /ssd-playpen/kzaman/student-teacher-interaction --output-folder results_dynamic_refinement/$CATEGORY/$SUBCAT --questions-folder data/b_questions/$CATEGORY/$SUBCAT --answers-folder data/c_answers/$CATEGORY/$SUBCAT --static-folder data/d_static/$CATEGORY/$SUBCAT #--results-folder results_dynamic_plain/$CATEGORY/$SUBCAT
+    done
+elif [ "$CATEGORY" == "academic_papers" ]; then
+    for SUBCAT in "${academic_papers[@]}"
+    do
+        #echo -e "$CATEGORY/$SUBCAT"
+        python 5-Dynamic/dynamic.py --context $CATEGORY --num-turns 5 --refine-questions --context-folder data/a_files/$CATEGORY/$SUBCAT --root-folder /ssd-playpen/kzaman/student-teacher-interaction --output-folder results_dynamic_refinement/$CATEGORY/$SUBCAT --questions-folder data/b_questions/$CATEGORY/$SUBCAT --answers-folder data/c_answers/$CATEGORY/$SUBCAT --static-folder data/d_static/$CATEGORY/$SUBCAT #--results-folder results_dynamic_plain/$CATEGORY/$SUBCAT
+    done
+else
+    #echo "$CATEGORY"
+    python 5-Dynamic/dynamic.py --context $CATEGORY --num-turns 5 --refine-questions --context-folder data/a_files/$CATEGORY --root-folder /ssd-playpen/kzaman/student-teacher-interaction --output-folder results_dynamic_refinement/$CATEGORY --questions-folder data/b_questions/$CATEGORY --answers-folder data/c_answers/$CATEGORY --static-folder data/d_static/$CATEGORY #--results-folder results_dynamic_plain/$CATEGORY/
+fi
+
+
+
