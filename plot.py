ee6c3e8ec909680639b04aa3c4c19baaa3ad03bc
KeremZaman <kerem_zaman@hotmail.com>
2024-05-08 13:04:47 -0400

    Add detailed token length analysis and make plots selectabel

--- plot.py
+++ plot.py
@@ -7,6 +7,24 @@ import os
 import json
 import argparse
 import glob
+import tiktoken
+import json
+import PyPDF2
+
+gpt_tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+def extract_text_from_pdf(filepath):
+    text = ""
+    with open(filepath, "rb") as file:
+        pdf = PyPDF2.PdfReader(file)
+        for page_num in range(len(pdf.pages)):
+            text += pdf.pages[page_num].extract_text()
+            if (
+                len(text) >= 1500
+            ):  # Check if the accumulated text has reached 1500 characters
+                break  # Stop reading further if 1500 characters have been reached
+    return text[:1500]  # Return only the first 1500 characters of the text
+
 
 def preprocess_dynamic_results(results_dir, method_name):
     all_results = []
@@ -50,24 +68,51 @@ def preprocess_all_results(static_results_dir, plain_results_dir, lesson_results
     all_results += preprocess_static_results(static_results_dir)
     return all_results
 
-def plot_static_lesson_lengths(static_lesson_dir, output_name):
+def plot_lengths(static_lesson_dir, content_dir, plain_results_dir, lesson_results_dir, refinement_results_dir, output_dir):
+    def _count_tokens(text):
+        return len(gpt_tokenizer.encode(text))
+
+    def _get_all_content(dir_name, document_type, context):
+        if document_type in ['plain', 'w_lesson', 'refinement']:
+            pattern = f'{dir_name}/{context}/**/chat_history_*.json'
+        elif context == 'academic_papers' and document_type == 'content':
+            pattern = f'{dir_name}/{context}/**/*.pdf'
+        else:
+            pattern = f'{dir_name}/{context}/**/*.md'
+
+        return glob.glob(pattern, recursive=True)
+
     lengths = []
     contexts = ['academic_papers', 'movie_plots', 'news_articles', 'song_lyrics']
+    document_types_to_dirs = {'static_lesson': static_lesson_dir, 
+                              'plain': plain_results_dir,
+                              'w_lesson': lesson_results_dir,
+                              'refinement': refinement_results_dir,
+                              'content': content_dir}
+    
     for context in contexts:
-        for fname in glob.glob(f'{static_lesson_dir}/{context}/**/*.md', recursive=True):
-            with open(fname, 'r') as f:
-                file_length = len(f.read())
-            lengths.append({'length': file_length, 'context': context})
+        for document_type, folder in document_types_to_dirs.items():
+            files = _get_all_content(folder, document_type, context)
+            for file in files:
+                with open(file, 'r') as f:
+                    if file.endswith('.json'):
+                        chat_data = json.load(f)
+                        content = ' '.join(list(map(lambda x: x['content'], chat_data)))
+                    elif file.endswith('.pdf'):
+                        content = extract_text_from_pdf(file)
+                    else:
+                        content = f.read()
+                content_length = _count_tokens(content)
+                lengths.append({'length': content_length, 'context': context, 'document': document_type}) 
 
     df = pd.DataFrame(lengths)
-    sns.barplot(df, x='context', y='length')
+    sns.barplot(df, x='context', y='length', hue='document')
 
     plt.tight_layout()
     plt.show()
-    plt.savefig(output_name, dpi=300)
+    plt.savefig(os.path.join(output_dir, 'lengths.png'), dpi=300)
 
 def plot_results(static_results_dir, plain_results_dir, lesson_results_dir, refinement_results_dir, static_lessons_dir, output_dir):
-    plot_static_lesson_lengths(static_lessons_dir, os.path.join(output_dir, 'lengths.png'))
     
     results = preprocess_all_results(static_results_dir, plain_results_dir, lesson_results_dir, refinement_results_dir)
     df = pd.DataFrame(results)
@@ -85,14 +130,17 @@ def plot_results(static_results_dir, plain_results_dir, lesson_results_dir, refi
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plot all results")
+    parser.add_argument("--plot", choices=["results", "lengths"], required=True)
     parser.add_argument("--static-results", help="Static results directory containing xlsx files")
     parser.add_argument("--plain-results", help="Plain results directory containing results.json file")
     parser.add_argument("--lesson-results", help="Lesson-informed results directory containing results.json file")
     parser.add_argument("--refinement-results", help="Refinement results directory containing results.json file")
     parser.add_argument("--static-lessons", help="Directory containing static lessons")
-    parser.add_argument("--output", help="Output directory for the plots")
+    parser.add_argument("--raw-contents", help="Directory containing raw contents")
+    parser.add_argument("--output", help="Output directory for the plots", required=True)
 
     args = parser.parse_args()
-
-    # Call the run() function with the provided arguments
-    plot_results(args.static_results, args.plain_results, args.lesson_results, args.refinement_results, args.static_lessons, args.output)
+    if args.plot == 'results':
+        plot_results(args.static_results, args.plain_results, args.lesson_results, args.refinement_results, args.static_lessons, args.output)
+    elif args.plot == 'lengths':
+        plot_lengths(args.static_lessons, args.raw_contents, args.plain_results, args.lesson_results, args.refinement_results, args.output)
